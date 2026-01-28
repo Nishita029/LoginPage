5353e3a7e15bcd2cb5496b651b87e4d3f6701d2c
NISHITA <nishitaietmca05@gmail.com>
2026-01-28 20:28:48 +0530

    added styling to the login component

--- login/src/components/login.jsx
+++ login/src/components/login.jsx
@@ -5,15 +5,24 @@ function Login(){
         <div className="loginContainer">
             <h1>Login</h1>
             <div>
-                <div>
-                <label>Email: </label>
-                <input type="email" placeholder='Enter email..' />
+                <div className='loginLabel'>
+                    <label>Email: </label>
                 </div>
-                
                 <div>
-                <label>Password: </label>
-                <input type="password" placeholder='Enter password ..' />
+                    <input className='inputField' type="email" placeholder='Enter email..' />
                 </div>
+                <div className='loginLabel'>
+                    <label>Password: </label>
+                </div>
+                <div>
+                    <input className='inputField' type="password" placeholder='Enter password ..' />
+                </div>
+
+                <div className='button-container'>
+                <button className='button'>signIn</button>
+                <button className='button'>signUp</button>
+                </div>
+
             </div>
         </div>
     );
