1e7bf1fedc6fa1e7e73d0300da665b9f2cf64c5a
NISHITA <nishitaietmca05@gmail.com>
2026-01-28 20:40:54 +0530

    added styling

--- login/src/components/login.jsx
+++ login/src/components/login.jsx
@@ -3,26 +3,27 @@ function Login(){
 
     return(
         <div className="loginContainer">
-            <h1>Login</h1>
-            <div>
-                <div className='loginLabel'>
-                    <label>Email: </label>
-                </div>
+            <div className="loginCard">
+                <h1>Login</h1>
                 <div>
-                    <input className='inputField' type="email" placeholder='Enter email..' />
-                </div>
-                <div className='loginLabel'>
-                    <label>Password: </label>
-                </div>
-                <div>
-                    <input className='inputField' type="password" placeholder='Enter password ..' />
-                </div>
+                    <div className='loginLabel'>
+                        <label>Email: </label>
+                    </div>
+                    <div>
+                        <input className='inputField' type="email" placeholder='Enter email..' />
+                    </div>
+                    <div className='loginLabel'>
+                        <label>Password: </label>
+                    </div>
+                    <div>
+                        <input className='inputField' type="password" placeholder='Enter password ..' />
+                    </div>
 
-                <div className='button-container'>
-                <button className='button'>signIn</button>
-                <button className='button'>signUp</button>
+                    <div className='button-container'>
+                        <button className='button'>signIn</button>
+                        <button className='button'>signUp</button>
+                    </div>
                 </div>
-
             </div>
         </div>
     );
